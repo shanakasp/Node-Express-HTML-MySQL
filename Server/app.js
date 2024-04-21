92dc4db87b7d6f7fb2feca5afca96ff64b5b241f
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-21 12:20:13 +0530

    modified app.js in Server

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,30 @@
-const app = express();
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+//middlware
+
+app.use((req, res, next) => {
+  console.log("API Path", req.path);
+  next();
+});
+
+//create
+app.post("/insert", (req, res) => {});
+
+//read
+app.get("/get", (req, res) => {
+  console.log("test");
+});
+
+//update
+app.put("/update", (req, res) => {});
+
+//delete
+app.delete("/delete", (req, res) => {});
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
